5ddd8ff7e1a8356de3f604ed5f88f19cbcc5e79d
PrplFox <foxedex29@gmail.com>
2024-01-05 20:17:17 +0100

    error handeling

--- src/wikifox.py
+++ src/wikifox.py
@@ -10,13 +10,12 @@ def clear_terminal():
 
 def select_language():
     print(f"\n{config.RESET}[0] English-en\n[1] Arabic-ar\n[2] Russian-ru\n[3] Spanish-es\n[4] Japanese-ja\n[5] German-de\n[6] French-fr\n[7] Italian-it\n[8] Cheinese-zh\n[9] Persian-fa")
-    language = int(input('\n\nPlease specify the language of search (use numbers) : '))
-    
-    if language not in list(range(10)):
-        print(f'{config.RED} Invalid language choice choose one of the languages on the list :\n {config.RESET}')
+    try:
+        language = int(input('\n\nPlease specify the language of search (use numbers) : '))
+        return config.LANGUAGES[language]
+    except (IndexError, ValueError):
+        print(f'{config.RED}Invalid language choice choose one of the languages on the list :{config.RESET}')
         return select_language()
-
-    return config.LANGUAGES[language]
 
 def search_link(language):
     search_input = input(f'{config.GREEN}What to search for in {language}?: ')
@@ -28,30 +27,38 @@ def search_link(language):
 def search_results(search_link, language):
     results = []
     page = get(search_link, headers=config.HEADERS)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    search_results = soup.find_all('li', class_='mw-search-result mw-search-result-ns-0')
-    
-    for result in search_results:
-        heading = result.find('div', class_='mw-search-result-heading').find('a')
-        title, link = heading.get('title'), heading.get('href')
-        wiki_link = f"https://{language}.wikipedia.org" + link
-        details = result.find('div', class_='searchresult').text
-        additional_info = result.find('div', class_='mw-search-result-data').text
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        search_results = soup.find_all('li', class_='mw-search-result mw-search-result-ns-0')
         
-        results.append( { 'title': title, 'link': wiki_link, 'details': details, 'additional_info': additional_info } )
-    
-    return results
+        for result in search_results:
+            heading = result.find('div', class_='mw-search-result-heading').find('a')
+            title, link = heading.get('title'), heading.get('href')
+            wiki_link = f"https://{language}.wikipedia.org" + link
+            details = result.find('div', class_='searchresult').text
+            additional_info = result.find('div', class_='mw-search-result-data').text
+            
+            results.append( { 'title': title, 'link': wiki_link, 'details': details, 'additional_info': additional_info } )
+        
+        return results
+    else:
+        print('Wikipedia timed out X-X')
+        return None
 
 def pick_result(results):
     for index, result in enumerate(results) :
         print(f"{config.GREEN}----------------- [{index}] - {result['title']} -----------------")
         print(f"{config.CYAN}details: {result['details']}")
         print(f"{config.RESET}additional info: {result['additional_info']}\n\n")
-        
-    choice = int(input(f"{config.MAGENTA}Wich result you want? ( choose by index ): "))
-    clear_terminal()
     
-    return results[choice]['link']
+    try:
+        choice = int(input(f"{config.MAGENTA}Wich result you want? ( choose by index ): "))
+        clear_terminal()
+        return results[choice]['link']
+    
+    except (IndexError, ValueError):
+        print('Invalid choice.')
+        pick_result(results)
 
 def final_result(wiki_link):
     page = get(wiki_link, headers=config.HEADERS)
@@ -64,9 +71,13 @@ def wikifox():
     language = select_language()
     wiki_search_link = search_link(language)
     results = search_results(wiki_search_link, language)
-    wiki_link = pick_result(results)
-    result = final_result(wiki_link)
-    print(result)
+    if results:
+        wiki_link = pick_result(results)
+        result = final_result(wiki_link)
+        print(result)
+    else:
+        print('Wikipedia timed out .')
+        wikifox()
     
 if __name__ == '__main__':
     wikifox()
